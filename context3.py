258866c73bc209b92f75dbe78e4b5c4ce4d5ca2d
林煥培 <abc2464831@gmail.com>
2020-10-10 19:47:07 +0800

    string can spilt too

---
+++ context3.py
@@ -0,0 +1,13 @@
+#字串也可分割喔～～
+
+lines = []
+with open('3.txt','r', encoding='utf-8-sig') as f:
+	for line in f:
+		lines.append(line.strip())
+
+for line in lines:
+	s = line.split()
+	time = s[0][:5]
+	name = s[0][5:]
+	print(name)
+	print(time)
